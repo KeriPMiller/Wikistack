efc5d0857e6e747d1e07a4bed2b5b99254364571
E H <ev@JMac.local>
2017-09-22 16:51:10 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+var bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+
+const env = nunjucks.configure('views', {noCache: true}); //where you find the fiews, caching off
+app.set('view engine', 'html');
+app.engine ('html', nunjucks.render);
+
+//log information about each incomming request
+app.use(morgan('dev'));
+
+//Body parsing middleware
+app.use(bodyParser.urlencoded({extended: true})); //for html form submits
+app.use(bodyParser.json()); //would be for ajax request
+
+//serves up static files from public folder
+app.use(express.static('public'));
+
+app.get('/', function (req, res)    {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+})
+
+//set up server
+var server = app.listen(1337, function ()  {
+    console.log('listening on port 1337');
+});
