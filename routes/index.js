8aabc57138baddfc7d714b3859c5773c43316457
E H <ev@JMac.local>
2017-09-25 17:37:53 -0400

    home page working

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,16 @@ const wikiRouter = require ('./wiki');
 const userRouter = require ('./user');
 
 
-    // router.use('/',function(req, res, next) {
-        
-    //     // models.Page.findAll()
-    //     // .then(inst => {
-    //     //     res.render('index', {inst});
-    //     // })
-    //     // .catch(next);
-    // });
     router.use('/wiki', wikiRouter);
     router.use('/user', userRouter);
-
+    router.use('/',function(req, res, next) {
+        
+        models.Page.findAll()
+        .then(inst => {
+            res.render('index', {pages: inst});
+        })
+        .catch(next);
+    });
+    
+    
 module.exports = router;
