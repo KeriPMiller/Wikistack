a507751131c45129e293412c0c65a5a8e29bba8d
E H <ev@JMac.local>
2017-09-25 16:56:50 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,14 @@ const wikiRouter = require ('./wiki');
 const userRouter = require ('./user');
 
 
-    router.use('/',function(req, res, next) {
-        return models.Page.findAll()
-        .then(inst => {
-            res.render('index',{
-                urlTitle: inst.getDataValue('urlTitle'),
-                title: inst.getDataValue('title')
-            })
-
-        })
-        .catch(next);
-    });
+    // router.use('/',function(req, res, next) {
+        
+    //     // models.Page.findAll()
+    //     // .then(inst => {
+    //     //     res.render('index', {inst});
+    //     // })
+    //     // .catch(next);
+    // });
     router.use('/wiki', wikiRouter);
     router.use('/user', userRouter);
 
