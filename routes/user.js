e7b9777b342afbc7ccdf0410cd9c4bc100023f7e
E H <ev@JMac.local>
2017-09-25 12:24:44 -0400

    add page view updated

--- routes/user.js
+++ routes/user.js
@@ -8,17 +8,22 @@ router.get('/', function (req, res, next)   {
     
 });
 router.get('/:id', function (req, res, next)   {
-    res.send("got to get /user/ with id ", req.params.id);
+    res.send("got to get /user/ with id " + req.params.id);
     
     
 });
 router.post('/', function (req, res, next)   {
+    res.send("got to get /user/");
+    
     
 });
 router.put('/:id', function (req, res, next)   {
+    res.send("got to post /user/ with id " + req.params.id);
+    
     
 });
 router.delete('/:id', function (req, res, next)   {
+    res.send("got to delete /user/ with id " + req.params.id);
     
 });
 
